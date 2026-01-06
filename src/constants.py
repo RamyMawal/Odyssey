5f12b6601fb332b253378633f2c0e81702c03a35
RamyMawal <r.mawal98@gmail.com>
2026-01-06 10:29:18 +0200

    update constants to fix a minor bug

--- src/constants.py
+++ src/constants.py
@@ -7,10 +7,15 @@ MARKER_LENGTH = 0.12  # in meters
 ALL_MARKER_IDS = np.array([0, 1, 2, 3])
 
 # Formation link configuration
-LINK_LENGTH = 0.25  # in meters
+LINK_LENGTH = 0.4  # in meters
 
 # Link-to-agent mapping
 LINK_AGENT_MAP = {0: [0], 1: [1, 2], 2: [3]}
 
 # Nominal offsets for each agent within their link frame
-NOMINAL_OFFSETS = {0: (0.0, 0.0), 1: (0.0, 0.0), 2: (LINK_LENGTH, 0.0), 3: (LINK_LENGTH, 0.0)}
+NOMINAL_OFFSETS = {
+    0: (0.0, 0.0),
+    1: (0.0, 0.0),
+    2: (LINK_LENGTH, 0.0),
+    3: (LINK_LENGTH, 0.0),
+}
